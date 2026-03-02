43ddf1ef5adfdfea5393107c44192cc229b65f4b
tom4k <tom4kurian@gmail.com>
2026-03-02 22:18:32 +0530

    preprcess opdate

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def main():
     file_path = 'data/data.csv'
+    output_path = 'data/preprocessed_data.csv'
     # Read the CSV data
     df = pd.read_csv(file_path)
     
@@ -9,8 +10,8 @@ def main():
     normalized_df = (df - df.min()) / (df.max() - df.min())
     
     # Save the normalized data back
-    normalized_df.to_csv(file_path, index=False)
-    print(f"Successfully normalized data in {file_path}")
+    normalized_df.to_csv(output_path, index=False)
+    print(f"Successfully normalized data in {output_path}")
 
 if __name__ == '__main__':
     main()
